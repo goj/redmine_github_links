fdb9820a7fcf6e9143e60304929a8190f6a892da
Krzysztof Goj <krzysztof.goj@erlang-solutions.com>
2010-08-12 16:15:45 +0200

    Fixed paths so this plug-in works on Harry

--- lib/github_links_application_hooks.rb
+++ lib/github_links_application_hooks.rb
@@ -2,7 +2,7 @@ require 'redmine'
 class GitHubLinksApplicationHooks < Redmine::Hook::ViewListener
 
   def view_layouts_base_html_head(context = {})
-    stylesheet_link_tag("/plugin_assets/redmine_github_links/stylesheets/github_links.css")
+    stylesheet_link_tag("/redmine/plugin_assets/redmine_github_links/stylesheets/github_links.css")
   end
 
 end
